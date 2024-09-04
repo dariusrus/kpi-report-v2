6a1d3b09a8b1794995d8e161d3456a6e65f2ac95
Darius Rus <33151655+dariusrus@users.noreply.github.com>
2024-09-04 18:54:16 +0300

    Add utility batch files
    Add utility batch files

---
+++ utility-packageProd.bat
@@ -0,0 +1,6 @@
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk-18.0.1.1
+echo setting PATH
+call mvnw clean
+call mvnw -DskipTests=true package
+pause
---
+++ utility-startDev.bat
@@ -0,0 +1,5 @@
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk-18.0.1.1
+echo setting PATH
+call mvnw clean spring-boot:run
+pause
