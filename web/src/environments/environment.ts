bd8c89cdb381bdb3e065dd4e43703cf2d5d6b282
Ian Culanag <ian@builderleadconverter.com>
2024-09-05 01:30:50 +0800

    Update backend API URLs

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   appConfig: {
     previousMonthsCount: 5
   },
-  apiUrl: 'http://localhost:8325/blc-kpi/reports',
+  apiUrl: 'https://api.builderleadconverter.com:8325/blc-kpi/reports',
   apiKey: '27fc2f8f489e4813a965114be60893d4'
 };
